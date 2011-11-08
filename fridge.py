8361de71e281211a9f8e6e7a1655fcb768a8d372
eri451 <hans.orter@gmx.de>
2011-11-08 23:49:24 +0100

    barcodes

--- fridge.py
+++ fridge.py
@@ -4,48 +4,76 @@ import json
 import sys
 
 fridge = {}
+barcode = {}
 product = ""
 action  = ""
 
 def main(args):
-    fridge  = load()
+    global fridge
+    fridge  = load("fridge.json")
     if len(args) <= 1:
         action  = raw_input("Operator = ")
         if action == "show":
             show_fridge()
+            return
+        elif action == "learn":
+            learn_barcode()
             return
         amount  = int(raw_input("Anzahl = "))
         product = raw_input("Produkt = ")
     elif args[1] == "show":
         show_fridge()
         return
+    elif args[1] == "learn":
+        learn_barcode()
     else:
         action  = args[1]
         amount = int(args[2])
         product = args[3]
     open_fridge(action, product, amount)
 
+def parse_barcode(product):
+    if product[0].isdigit():
+        global barcode
+        barcode = load("barcode.json")
+        return barcode[product]
+    else:
+        print "could not convert please use learn first next time"
+        return product
+
+def learn_barcode():
+    product = raw_input("Barcode: ")
+    name = raw_input("Name: ")
+    global barcode
+    barcode = load("barcode.json")
+    barcode.update({product:name})
+    print barcode
+    save(barcode, "barcode.json")
+
 def open_fridge(action, product, amount):
     if action == "add":
         for i in range(amount):
             add_product(product)
-        print str(amount) + " " + product + " hinzugefuegt"
-        save(fridge)
+        productname = parse_barcode(product)
+        print str(amount) + " " + productname + " hinzugefuegt"
+        save(fridge,"fridge.json")
     elif action == "sub":
         if (not(fridge.has_key(product))):
             print "erst reinlegen dann raus nehmen!!"
             return
         for i in range(amount):
             sub_product(product)
-        print str(amount) + " " + product + "rausgenommen"
-        print str(fridge[product]) + " " + product + "verbleiben"
-        save(fridge)
+        productname = parse_barcode(product)
+        print str(amount) + " " + productname + " rausgenommen"
+        print str(fridge[product]) + " " + productname + " verbleiben"
+        save(fridge,"fridge.json")
     else:
          print "falsche Eingabe. Bitte nutze \"show\", \"add\" oder \"sub\"."
 
 def show_fridge():
-    for product in fridge.keys():
-        print "" + product + " " + str(fridge[product])
+    for barcodes in fridge.keys():
+        product = parse_barcode(barcodes)
+        print "" + product + ": " + str(fridge[barcodes])
 
 
 def add_product(product):
@@ -61,21 +89,23 @@ def sub_product(product):
     else:
         print "Fridge leer!"
 
-def save(fridge):
-    f = open("fridge.json", "w")
-    json.dump(fridge,f)
+def save(d,filename):
+    f = open(filename, "w")
+    json.dump(d,f)
     f.close()
 
-def load():
+def load(filename):
     try:
-        global fridge
-        f = open("fridge.json","r")
-        fridge = json.load(f)
+        d = {}
+        f = open(filename,"r")
+        d = json.load(f)
         f.close()
     except:
-        fridge = {}
-    finally:
-        return fridge
+        print "created " + filename
+        f = open(filename,"w")
+        f.close()
+        load(filename)
+    return d
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
