8b25d3d8bade37c3937ad2d66feb310b3b80e3c9
shithead <pwnytail@c3d2.de>
2013-06-08 02:24:06 +0200

    create barcodereader class

--- barcode.py
+++ barcode.py
@@ -4,6 +4,42 @@ import sys
 import json
 import argparse
 
+class barcodereader:
+    """Simple Class for abarcodereader"""
+    def __init__(self):
+        self.JsonFileContent = {}
+        self.JsonFilename= ""
+
+    def ParserJson(self, item):
+        if item[0].isdigit():
+            return self.JsonFileContent[item]
+        else:
+            print "could not convert please use learn first next time"
+            return item
+
+    def LearnCodeJson(self, item, name):
+        self.JsonFileContent.update({item:name})
+        print "learned "+item+" is "+name
+
+    def Load4Json(self, json_filename):
+        try:
+            fPtr = open(json_filename, 'r')
+        except IOError:
+            fPtr = open(filename,"w")
+            print "created " + filename
+        else:
+            self.jsonFileContent = json.load(fPtr)
+        finally:
+            fPtr.close()
+            self.JsonFilename = json_filename
+
+    def Save2Json(self, json_filename):
+        fPtr = open(json_filename, 'w')
+        json.dump(self.JsonFileContent, fPtr)
+        fPtr.close()
+        self.JsonFilename = json_filename
+
+
 def parser(product):
     if product[0].isdigit():
         known_product = load("barcode.json")[product]
